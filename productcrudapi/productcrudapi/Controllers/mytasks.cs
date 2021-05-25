e44578752f6ddbfb0503262dbe71a33a64041c42
Saumya Srivastava <saumya@git.com>
2021-05-25 07:20:30 -0700

    EF Controller ,CRUD Operation

---
+++ productcrudapi/productcrudapi/Controllers/mytasks.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using productcrudapi.Infrastructure;
+using productcrudapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace productcrudapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class mytasks : ControllerBase
+    {
+
+        private readonly TodoItemContext _context;
+
+        public mytasks(TodoItemContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("items")]
+        public List<TodoItem> getItems()
+        {
+            var result = from r in _context.TodoItems select r;
+            return result.ToList();
+        }
+
+        [HttpPost("items")]
+        public TodoItem createItem(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+            return item;
+        }
+
+        [HttpPut("items/{id}")]
+        public TodoItem updateItem(TodoItem item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+            return item;
+        }
+    }
+}
---
+++ productcrudapi/productcrudapi/Infrastructure/TodoItemContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using productcrudapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace productcrudapi.Infrastructure
+{
+    public class TodoItemContext : DbContext
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        public TodoItemContext(DbContextOptions<TodoItemContext> options) : base(options)
+        {
+
+        }
+    }
+}
